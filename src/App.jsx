c406a87b21b455f3ccf1343da62df7de54511e13
tanyamedina <criaturamaldita@gmail.com>
2023-11-12 21:29:57 -0700

    Secciones de menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // Estado local del componente para gestionar la visibilidad del menú
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  // Manejador de eventos para alternar la visibilidad del menú
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div className="container">
       <h1>Proyecto II</h1>
       <h2>Producto integrador. El final del proceso</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <button onClick={toggleMenu}>Menú</button>
+
+      {menuVisible && (
+        <ul>
+          <li>Actividades realizadas en el curso </li>
+          <li>Código fuente (comprimido) </li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default App;
