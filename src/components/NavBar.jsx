b1d54bcf22aa817ed0549876e90cea20406a08d4
tanyamedina <criaturamaldita@gmail.com>
2023-11-17 08:21:29 -0700

    Arreglo de problemas con botones de menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,6 +13,13 @@ export const NavBar = () => {
     setMenuVisible(false);
   };
 
+  const handleDownloadSourceCode = () => {
+    // Ruta correcta al archivo ZIP
+    const downloadUrl = '/files/sitio-web-cecmex.zip';
+    window.open(downloadUrl);
+    handleCloseMenu();
+  };
+
   return (
     <nav>
       <div className="popup-container">
@@ -22,14 +29,17 @@ export const NavBar = () => {
             <button onClick={() => { navigate("/Home"); handleCloseMenu(); }}>Home</button>
             {/* Enlace directo al PDF */}
             <a href="/files/U4-Actividades-realizadas.pdf" target="_blank" rel="noopener noreferrer" onClick={handleCloseMenu}>
-              Actividades realizadas</a>
-            <button onClick={() => { navigate("/opcion2"); handleCloseMenu(); }}>Descargar código fuente</button>
+              <button>Actividades realizadas</button>
+            </a>
+            <button onClick={handleDownloadSourceCode}>Descargar código fuente</button>
             <button onClick={() => { navigate("/opcion3"); handleCloseMenu(); }}>Sistema implementado</button>
             <button onClick={() => { navigate("/opcion3"); handleCloseMenu(); }}>Reporte de modificaciones</button>
-            <button onClick={() => { navigate("/opcion3"); handleCloseMenu(); }}>Ir al repositorio de GitHub</button>
+            <a href="https://github.com/dianarojo/sitio-web-cecmex.git" target="_blank" rel="noopener noreferrer">
+              <button>Ir al repositorio de GitHub</button>
+            </a>
           </div>
         )}
       </div>
     </nav>
   );
-}; 
+};
