f31021b45d7ce08679c172588864ec06952a59b9
tanyamedina <criaturamaldita@gmail.com>
2023-11-18 07:56:41 -0700

    Intento cambiando config index 4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: 'src/main.js', // Asegúrate de que sea la ruta correcta a tu archivo principal
+    },
+  },
+});
+
